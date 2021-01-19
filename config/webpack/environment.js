d510a6fffcd930a8c4fa33c67fb5e8c25337427e
Gabriel Walsh <gabriel.walsh@moleskine.com>
2021-01-19 09:27:24 -0500

    hot reload working

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,17 @@
 const { environment } = require('@rails/webpacker')
+
 const webpack = require('webpack')
 
-environment.plugins.prepend(
-    'Provide',
-    new webpack.ProvidePlugin({
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
         $: 'jquery',
-        jQuery: 'jQuery',
-        jquery: 'jquery',
-        'window.jQuery': 'jquery',
+        jQuery: 'jquery',
         Popper: ['popper.js', 'default']
-    })
-)
+}));
 
-module.exports = environment
+const config = environment.toWebpackConfig();
+
+config.resolve.alias = {
+        jquery: 'jquery/src/jquery'
+};
+
+module.exports = environment;
