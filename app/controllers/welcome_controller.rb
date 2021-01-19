a945a454075c614c93a2972aeacc9822fbe6a86f
Gabriel Walsh <gabriel.walsh@moleskine.com>
2021-01-19 09:20:25 -0500

    switching

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+
+  def home
+  end
+
+  def new_book
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+  get 'welcome/new_book'
   root 'pages#home'
   devise_for :users
 end
