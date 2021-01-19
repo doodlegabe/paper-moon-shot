9a8aa58f8637003f88f3cde3242b1e9d81ef325d
Gabriel Walsh <gabriel.walsh@moleskine.com>
2021-01-19 11:19:43 -0500

    rough outline

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,21 @@ class WelcomeController < ApplicationController
   end
 
   def new_book
+  end
 
+  def new_page
+  end
+
+  def new_worksheet
+  end
+
+  def new_feedback_collector
+  end
+
+  def back_to_dashboard
+  end
+
+  def new_message
   end
 
 end
